12ba753dff8fc7bf3988df2bdb9fe6e4510d571d
alexboliam <alexboliam@gmail.com>
2024-07-24 22:24:01 +0300

    Set up Firebase, emulators and basic configs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'demo-project',
+    appId: 'APP_ID',
+    storageBucket: 'STORAGE_BUCKET',
+    apiKey: 'API_KEY',
+    authDomain: 'AUTH_DOMAIN',
+    messagingSenderId: 'MESSAGING_SENDER_ID',
+  },
+  production: false,
+  useEmulators: true,
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'PROJECT_ID',
+    appId: 'APP_ID',
+    storageBucket: 'STORAGE_BUCKET',
+    apiKey: 'API_KEY',
+    authDomain: 'AUTH_DOMAIN',
+    messagingSenderId: 'MESSAGING_SENDER_ID',
+  },
+  production: true,
+  useEmulators: false,
+};
